7402ac04bc0e7a922a5972d4e8ba4c98f974c8ed
Jean Max S Krebs <jeanmaxskrebs@gmail.com>
2020-12-24 17:21:10 -0300

    Adicionado MYSQL e configurações

--- app.js
+++ app.js
@@ -5,20 +5,40 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("kkkkkkkkkkkkkkkkkkkkkkk");
+//conexao sql
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'tcc',
+    password: '123456',
+    database: 'tcc'
+});
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
 });
 
-app.get("/home", function(req, res){
-    res.send("Página Home");
+
+
+app.get("/", function (req, res) {
+    res.sendFile("E:/Projeto TCC/src/index.html");
 });
 
-app.get("/sobre", function(req, res){
-    res.send("Página Sobre");
+app.get("/home", function (req, res) {
+    res.sendFile("E:/Projeto TCC/src/index.html");
 });
 
-app.get("/contato", function(req, res){
-    res.send("Página Contato");
+app.get("/sobre", function (req, res) {
+    res.sendFile("E:/Projeto TCC/src/sobre.html");
+});
+
+app.get("/contato", function (req, res) {
+    res.sendFile("E:/Projeto TCC/src/contato.html");
 });
 
 app.listen(3000);
